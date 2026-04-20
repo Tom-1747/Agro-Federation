1e96bc1aedde14b8959d7b0894a86ac145521e7f
Tom-1747 <hei.damien.3@gmail.com>
2026-04-20 11:28:50 +0300

    Initial commit

---
+++ src/main/java/com/example/agricultureFederation/AgricultureFederationApplication.java
@@ -0,0 +1,13 @@
+package com.example.agricultureFederation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricultureFederationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricultureFederationApplication.class, args);
+	}
+
+}
